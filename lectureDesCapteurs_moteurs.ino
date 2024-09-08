8a740496de3dd3178076b4e436d1938a55827444
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2024-09-08 03:24:57 +0200

    Create lectureDesCapteurs_moteurs.ino

---
+++ lectureDesCapteurs_moteurs.ino
@@ -0,0 +1,90 @@
+#include <DHT.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+#define trigPin 9
+#define echoPin 10
+#define motor1Pin1 2
+#define motor1Pin2 3
+#define motor2Pin1 4
+#define motor2Pin2 5
+#define DHTPIN 6
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+TinyGPSPlus gps;
+SoftwareSerial ss(4, 3); // RX, TX
+
+void setup() {
+  Serial.begin(9600);
+  ss.begin(9600);
+  dht.begin();
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
+}
+
+void loop() {
+  // Lecture des données du capteur de distance
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
+
+  // Lecture des données du capteur de température et d'humidité
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // Lecture des données GPS
+  while (ss.available() > 0) {
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()) {
+      Serial.print("Latitude: ");
+      Serial.println(gps.location.lat(), 6);
+      Serial.print("Longitude: ");
+      Serial.println(gps.location.lng(), 6);
+    }
+  }
+
+  // Affichage des données sur le moniteur série
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  Serial.print("Humidité: ");
+  Serial.print(h);
+  Serial.println(" %");
+  Serial.print("Température: ");
+  Serial.print(t);
+  Serial.println(" °C");
+
+  // Contrôle des moteurs en fonction de la distance
+  if (distance < 20) {
+    // Obstacle détecté, reculer
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, HIGH);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, HIGH);
+    delay(500);
+    // Tourner à droite
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, HIGH);
+    delay(500);
+  } else {
+    // Avancer
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, HIGH);
+    digitalWrite(motor2Pin2, LOW);
+  }
+
+  delay(1000);  // Attente de 1 seconde avant la prochaine lecture
+}
